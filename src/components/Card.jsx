9a9a524c12264f23915a94fa7cc0922bad6f55a9
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-29 11:27:10 +0200

    🐛 Fix warning in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,16 @@ import { Link } from 'react-router-dom';
 import data from '../data/data.json';
 export const Card = () => {
 	return (
-		// Component.js
 		<div className="page-container">
 			<div className="card-container">
 				{data.map(item => (
-					<Link key={data.id} to={`/product/${item.id}`} className="link-style">
+					<Link key={item.id} to={`/product/${item.id}`} className="link-style">
 						<img
 							src={item.cover}
 							alt="Image Appartement à louer"
 							className="card-image"
-							key="idImg"
 						/>
-						<p className="card-title" key="id-title">
-							{item.title}
-						</p>
+						<p className="card-title">{item.title}</p>
 					</Link>
 				))}
 			</div>
